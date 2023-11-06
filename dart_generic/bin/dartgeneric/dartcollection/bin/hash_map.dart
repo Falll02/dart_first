8a7e800bf51f20fc66d4699b9d3acdc89a5a738a
Falll02 <rizkannaufal2@gmail.com>
2023-11-06 20:25:53 +0800

    tugas mobile

---
+++ dart_generic/bin/dartgeneric/dartcollection/bin/hash_map.dart
@@ -0,0 +1,17 @@
+import 'dart:collection';
+
+void main(){
+
+  final scores = HashMap<String, int>();
+
+  scores["Hidayat"] = 100;
+  scores["Saputra"] = 100;
+  scores["Hafiz"] = 100;
+  scores["Ansyari"] = 100;
+  scores["Doyok"] = 100;
+  scores["Zenal"] = 100;
+  scores["Windah"] = 100;
+
+  print(scores);
+
+}
---
+++ dart_generic/bin/dartgeneric/dartcollection/bin/linked_hash_map.dart
@@ -0,0 +1,18 @@
+import 'dart:collection';
+
+void main(){
+
+  // final scores = Map<String, int>();
+  final scores = LinkedHashMap<String, int>();
+
+  scores["Hidayat"] = 100;
+  scores["Saputra"] = 100;
+  scores["Hafiz"] = 100;
+  scores["Ansyari"] = 100;
+  scores["Doyok"] = 100;
+  scores["Zenal"] = 100;
+  scores["Windah"] = 100;
+
+  print(scores);
+
+}
